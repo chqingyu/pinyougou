75dcbe32233b947a45b36ffd41f5bd01b0be68f8
cqy151 <413565455@qq.com>
2019-08-04 14:26:31 +0800

    abc

--- pinyougou-search-web/target/pinyougou-search-web-1.0-SNAPSHOT/js/base.js
+++
@@ -1,9 +0,0 @@
-var app=angular.module('pinyougou',[]);
-// 定义过滤器
-app.filter('trustHtml',['$sce',function($sce){
-	
-	// data是被过滤的内容
-	return function(data){
-		return $sce.trustAsHtml(data); // 返回的是过滤后的的内容(信任html)
-	}
-}]);
